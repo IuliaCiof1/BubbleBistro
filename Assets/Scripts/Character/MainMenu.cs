5ddfbeb0770512e47e221f42f5d546a6dd896e70
Odashi1 <150281861+Odashi1@users.noreply.github.com>
2025-01-25 15:58:49 +0200

    Main Menu
    Am implementat main menu cu functionalitati,

---
+++ Assets/Scripts/Character/MainMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    #region - Declarations
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+    [SerializeField] private GameObject VolumeUI;
+    [SerializeField] private GameObject MenuUI;
+    #endregion
+
+    #region - Default Methods
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    #endregion
+
+    #region - Methods
+    public void Play()
+    {
+        StartCoroutine(FadeOutAndLoadScene("Main"));
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color color = fadeImage.color;
+        color.a = 0;
+        fadeImage.color = color;
+
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Clamp01(timer / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ToggleVolumeMenu(bool showVolume)
+    {
+        VolumeUI.SetActive(showVolume);
+        MenuUI.SetActive(!showVolume);
+    }
+    #endregion
+
+}
